b5a83e0aabf2bc93c3af5e5b15003e7cb111916b
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-09-05 18:32:26 +0300

    navbar to be completed

--- Awards/urls.py
+++ Awards/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url,include
-from django.contrib.auth import views 
+from django.contrib.auth import views
+from django.contrib.auth.decorators import login_required
+
 
 
 urlpatterns = [
