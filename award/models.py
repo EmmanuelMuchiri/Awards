6d9ce82610d3a14fe166565dffe86bbc9b3e3cfb
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-09-06 15:38:49 +0300

    created class project and made migrations to the database

--- award/models.py
+++ award/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
+import datetime as dt
 
 # Create your models here.
 class Profile(models.Model):
@@ -12,3 +13,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=60)
+    post = HTMLField()
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    project_image = models.ImageField(upload_to='projects/', blank=True)
